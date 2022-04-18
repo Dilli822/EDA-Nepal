38250d414806c09fd5dc23428f98b17b1def3c77
dilli <dillihangrae@gmail.com>
2022-04-18 15:46:22 +0545

    [added] adds country-ts added

---
+++ country-ts/src/App.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import { Modal, Button, Breadcrumb } from 'antd';
+import logo from './logo.svg';
+import './App.css';
+
+const [isModalVisible, setIsModalVisible] = useState(false);
+function App() {
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleOk = () => {
+    setIsModalVisible(false);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+
+  return (
+    <div className="App">
+      <Button type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Modal>
+
+    <Breadcrumb separator=">">
+    <Breadcrumb.Item>Home</Breadcrumb.Item>
+    <Breadcrumb.Item href="">Application Center</Breadcrumb.Item>
+    <Breadcrumb.Item href="">Application List</Breadcrumb.Item>
+    <Breadcrumb.Item>An Application</Breadcrumb.Item>
+  </Breadcrumb>
+    </div>
+  );
+}
+
+export default App;
